d31fdaeb66cc94614469e98b9c54ddfcb3197f28
Vale <oirelav95@gmail.com>
2022-12-29 16:20:09 +0100

    aggiungo bottone upgrade nell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,8 +9,10 @@ export default {
 
 <template>
 
+    <!-- Header -->
     <header>
-        Sono un header
+        <!-- Bottone -->
+        <a class="button-upgrade" href="#">EFFETTUA L'UPGRADE</a>
     </header>
 
 </template>
@@ -28,5 +30,21 @@ header {
     background-color: #080b12;
     height: 90px;
     padding-right: 20px;
+
+    .button-upgrade {
+        text-align: center;
+        border-radius: 20px;
+        border: 1px solid #fff;
+        padding: 12px 35px;
+        line-height: 20px;
+        font-size: 0.75rem;
+        font-weight: 700;
+        color: #fff;
+        display: block;
+
+        &:hover {
+            transform: scale(1.08);
+        }
+    }
 }
 </style>
