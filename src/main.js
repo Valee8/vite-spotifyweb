a6132abd277d1091bd5e2879161205a408b12351
Vale <oirelav95@gmail.com>
2022-12-29 16:56:10 +0100

    importo libreria fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(faPlus)
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
