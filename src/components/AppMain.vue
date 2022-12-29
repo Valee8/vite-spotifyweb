9daf9859a7c8406f4b3acaebb0dc8cd5bb0bfc78
Vale <oirelav95@gmail.com>
2022-12-29 16:08:43 +0100

    creo componente AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    name: "AppMain",
+}
+
+</script>
+
+
+<template>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+@use './src/styles/general.scss' as *;
+@use './src/styles/partials/variables' as *;
+
+main {
+    height: calc(100vh - 180px);
+    overflow-y: auto;
+    padding: 20px;
+}
+</style>
