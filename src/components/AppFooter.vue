fc9653621ea56996c1b5f253bf525d85f25f4d75
Vale <oirelav95@gmail.com>
2022-12-29 15:46:34 +0100

    creo componente AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: "AppFooter",
+}
+
+</script>
+
+
+<template>
+
+    <footer>
+        Sono un footer
+    </footer>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
