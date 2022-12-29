56c56e9a66c7be37c85c3865e37caaeba1b23e51
Vale <oirelav95@gmail.com>
2022-12-29 16:09:08 +0100

    aggiungo css header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,4 +20,13 @@ export default {
 <style lang="scss" scoped>
 @use './src/styles/general.scss' as *;
 @use './src/styles/partials/variables' as *;
+
+header {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: #080b12;
+    height: 90px;
+    padding-right: 20px;
+}
 </style>
