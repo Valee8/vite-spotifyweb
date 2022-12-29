9cd57fd1127ecda3fa0ee3362f762b1ed92441c1
Vale <oirelav95@gmail.com>
2022-12-29 16:08:52 +0100

    aggiungo css footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -20,4 +20,15 @@ export default {
 <style lang="scss" scoped>
 @use './src/styles/general.scss' as *;
 @use './src/styles/partials/variables' as *;
+
+footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #282828;
+    height: 90px;
+    width: 100%;
+    color: #fff;
+    padding: 0 10px;
+}
 </style>
