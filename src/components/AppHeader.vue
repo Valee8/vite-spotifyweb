9b5e2083d87dfe0a203c7d4076c884447f9077fb
Vale <oirelav95@gmail.com>
2022-12-29 15:46:47 +0100

    creo componente AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: "AppHeader",
+}
+
+</script>
+
+
+<template>
+
+    <header>
+        Sono un header
+    </header>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
