e0a914dcbbefbf597bf6f707c2aa18f12cd9909e
Vale <oirelav95@gmail.com>
2022-12-29 17:44:41 +0100

    creo componente NavMenuCenter

---
+++ src/components/NavMenuCenter.vue
@@ -0,0 +1,74 @@
+<script>
+
+export default {
+    name: "NavMenuCenter",
+    data() {
+        return {
+            linksMenuCenter: [
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                },
+                {
+                    text: 'Nome Playlist',
+                    url: '#',
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+
+<template>
+
+    <!-- Link menu -->
+    <ul class="menu-links">
+
+        <li v-for="(links, index) in linksMenuCenter" :key="index">
+            <a :href="links.url">{{ links.text }}</a>
+        </li>
+    </ul>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use './src/styles/general.scss' as *;
+@use './src/styles/partials/variables' as *;
+
+.menu-links {
+    flex-grow: 1;
+    overflow-y: auto;
+    font-weight: 700;
+
+    a {
+        color: #676767;
+
+        &:hover {
+            color: #fff;
+        }
+    }
+}
+</style>
