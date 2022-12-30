4c6c99c905bb684380aa899b2c1f347797e9bf34
Vale <oirelav95@gmail.com>
2022-12-30 12:43:56 +0100

    importo nuove icone

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faPlus, faCircleArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faCircleArrowDown, faShuffle, faBackwardStep, faForwardStep, faArrowRotateRight, faList, faDesktop, faVolumeLow } from '@fortawesome/free-solid-svg-icons';
 
-import { faHeart, faFolder } from '@fortawesome/free-regular-svg-icons';
+import { faHeart, faFolder, faCirclePlay } from '@fortawesome/free-regular-svg-icons';
 
 /* add icons to the library */
-library.add(faPlus, faCircleArrowDown, faHeart, faFolder)
+library.add(faPlus, faCircleArrowDown, faHeart, faFolder, faShuffle, faBackwardStep, faForwardStep, faCirclePlay, faArrowRotateRight, faList, faDesktop, faVolumeLow)
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
