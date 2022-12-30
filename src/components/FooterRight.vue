f391f5846883dcbfea82ae8461323dfcd23f55bd
Vale <oirelav95@gmail.com>
2022-12-30 12:44:44 +0100

    creo componente FooterRight

---
+++ src/components/FooterRight.vue
@@ -0,0 +1,71 @@
+<script>
+
+export default {
+    name: "FooterRight",
+    data() {
+        return {
+            listIcons: [
+                {
+                    icon: 'fa-solid fa-list',
+                    url: '#'
+                },
+                {
+                    icon: 'fa-solid fa-desktop',
+                    url: '#'
+                },
+                {
+                    icon: 'fa-solid fa-volume-low',
+                    url: '#'
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+
+<template>
+
+    <!-- Parte destra footer -->
+    <div class="footer-right">
+
+        <!-- Inizio lista -->
+        <ul class="list-footer">
+            <li v-for="(icons, index) in listIcons" :key="index">
+                <a :href="icons.url">
+                    <font-awesome-icon :icon="icons.icon" />
+                </a>
+            </li>
+
+            <li class="bar">
+                <a href="#">
+                    <div class="bar-completed">
+                        <div class="bar-circle"> </div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+
+
+
+</template>
+
+<style lang="scss" scoped>
+@use './src/styles/general.scss' as *;
+@use './src/styles/partials/variables' as *;
+
+.footer-right {
+    width: 200px;
+
+    a {
+        color: #fff;
+    }
+
+    .list-footer {
+        gap: 17px;
+    }
+}
+</style>
