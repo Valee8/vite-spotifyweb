3217a041d21fcc5feed206421e08f56a308736df
Vale <oirelav95@gmail.com>
2023-01-08 18:55:12 +0100

    aggiungo array moreListened in store

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import { reactive } from 'vue'
 export const store = reactive({
     albums: [],
     artists: [],
+    moreListened: [],
     apiURL: "https://shazam.p.rapidapi.com/albums/get-related-artist"
 });
