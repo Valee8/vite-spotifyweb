2e06c335e77236ca044f48e61460b701903c82ff
Vale <oirelav95@gmail.com>
2023-01-08 17:06:00 +0100

    sposto contenuto da RecentlyPlayed a App

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import AppFooter from './components/AppFooter.vue'
 import AppNav from './components/AppNav.vue'
 import AppMain from './components/AppMain.vue'
 
+import axios from 'axios';
+
+import { store } from './store.js';
 
 export default {
   components: {
@@ -11,6 +14,51 @@ export default {
     AppFooter,
     AppNav,
     AppMain
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getAlbums() {
+      const options = {
+        method: 'GET',
+        url: 'https://spotify81.p.rapidapi.com/albums',
+        params: { ids: '6PFPjumGRpZnBzqnDci6qJ,4Gfnly5CzMJQqkUFfoHaP3,2ANVost0y2y52ema1E9xAZ,6mUdeDZCsExyJLMdAfDuwh,6i6folBtxKV28WX3msQ4FE,4isNDcdh05jGd91v2h6tbC' },
+        headers: {
+          'X-RapidAPI-Key': 'baf622d4edmshd85bb4f97fcc4aep15feaajsne026f2c5830b',
+          'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        store.albums = response.data.albums;
+      }).catch(function (error) {
+        console.error(error);
+      });
+    },
+    getArtists() {
+      const options = {
+        method: 'GET',
+        url: 'https://spotify81.p.rapidapi.com/artists',
+        params: { ids: '6XyY86QOPPrYVGvF9ch6wz,3fMbdgg4jU18AjLCKBhRSm,711MCceyCBcFnzjGY4Q7Un,1dfeR4HaWDbWqFHLkxsg1d' },
+        headers: {
+          'X-RapidAPI-Key': 'baf622d4edmshd85bb4f97fcc4aep15feaajsne026f2c5830b',
+          'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        store.artists = response.data.artists;
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+  },
+  mounted() {
+    this.getAlbums();
+    this.getArtists();
   }
 }
 </script>
@@ -68,6 +116,75 @@ main {
 
   img {
     max-width: 100%;
+  }
+
+  section {
+    h2 {
+      color: #fff;
+      font-size: 1.4rem;
+      padding-top: 40px;
+    }
+
+    .subtitle {
+      color: #7C7E83;
+      font-size: 0.7rem;
+      font-weight: 700;
+      padding-top: 5px;
+    }
+
+    .list-cards {
+      display: flex;
+      flex-wrap: wrap;
+      padding-top: 30px;
+      color: #fff;
+
+      .card {
+        width: calc(100% / 6);
+        text-align: center;
+        padding-right: 15px;
+        padding-bottom: 25px;
+
+        .subtitle {
+          padding-top: 0;
+        }
+
+        img {
+          height: 193px;
+          object-fit: cover;
+        }
+
+        h3 {
+          color: #fff;
+          font-size: 1rem;
+          padding: 10px 5px 0 5px;
+        }
+
+        .card-img {
+          position: relative;
+
+          .icon-circle-play {
+
+            color: #fff;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 3.5rem;
+            display: none;
+          }
+
+          &:hover {
+            img {
+              filter: brightness(83%);
+            }
+
+            .icon-circle-play {
+              display: block;
+            }
+          }
+        }
+      }
+    }
   }
 }
 
