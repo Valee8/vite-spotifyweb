dd5f58e0c8341cd93e621cc9fe371e8ea0d6113c
Vale <oirelav95@gmail.com>
2023-01-08 17:06:47 +0100

    creo componente FavoriteArtists

---
+++ src/components/FavoriteArtists.vue
@@ -0,0 +1,54 @@
+<script>
+
+import { store } from '../store.js';
+
+export default {
+    name: "FavoriteArtists",
+    data() {
+        return {
+            store,
+        }
+    },
+}
+
+</script>
+
+
+<template>
+
+    <section>
+        <!-- Titolo -->
+        <h2>
+            Artisti preferiti
+        </h2>
+
+        <!-- Testo -->
+        <div class="subtitle">
+            Pi&ugrave; ascolti, pi&ugrave; saranno accurati i suggerimenti.
+        </div>
+
+        <!-- Lista card -->
+        <div class="list-cards">
+            <!-- Card -->
+            <div class="card" v-for="(artists, index) in store.artists" :key="index">
+
+                <a :href="artists.external_urls.spotify" target="_blank">
+                    <img :src="artists.images[0].url">
+
+                    <h3>
+                        {{ artists.name }}
+                    </h3>
+                </a>
+
+            </div>
+        </div>
+
+    </section>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use './src/styles/general.scss' as *;
+@use './src/styles/partials/variables' as *;
+</style>
