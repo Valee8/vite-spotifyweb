79469d069247fa2e0431c34274504f0289c3f31f
Vale <oirelav95@gmail.com>
2023-01-08 18:57:25 +0100

    creo componente MostListened

---
+++ src/components/MostListened.vue
@@ -0,0 +1,49 @@
+<script>
+
+import { store } from '../store.js';
+
+export default {
+    name: "MostListened",
+    data() {
+        return {
+            store,
+        }
+    },
+}
+
+</script>
+
+
+<template>
+
+    <section>
+        <!-- Titolo -->
+        <h2 class="title">
+            Artisti pi&ugrave; ascoltati nell'ultimo mese
+        </h2>
+
+        <!-- Lista card -->
+        <div class="list-cards">
+            <!-- Card -->
+            <div class="card" v-for="(trend, index) in store.moreListened" :key="index">
+
+                <h1>
+                    {{ trend.rank }}
+                </h1>
+
+                <h3>
+                    {{ trend.artist }}
+                </h3>
+
+            </div>
+        </div>
+
+    </section>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use './src/styles/general.scss' as *;
+@use './src/styles/partials/variables' as *;
+</style>
