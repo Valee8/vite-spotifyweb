0b1fe551ec1f0310fc2b563419bb61eb262a2b57
Vale <oirelav95@gmail.com>
2023-01-10 00:10:33 +0100

    modifico valori store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    albums: [],
-    artists: [],
-    moreListened: [],
-    apiURL: "https://shazam.p.rapidapi.com/albums/get-related-artist"
+    cardList: [],
+    search: '',
+    options: ''
 });
